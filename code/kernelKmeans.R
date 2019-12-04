cdff5b3a7ad932ad1dce9c29267aba6d5d2bdf8e
mariagk <maria.gotsopoulou@hotmail.com>
2019-12-04 18:04:16 +0100

    added another alternative for rbf kernel function with spirals tdataset testing

--- code/kernelKmeans.R
+++ code/kernelKmeans.R
@@ -31,7 +31,7 @@ kernel <- switch(kernelFun,
 
 K <- kernelMatrix(rbf, data[,1:2])
 
-###### alternative func rbf & kernel matrix ###### 
+###### Option A: alternative func rbf & kernel matrix ###### 
 
 ## Euclidean Distance  
 #1.|| a || = sqrt(aDOTa), 
@@ -52,14 +52,61 @@ rbf<-function(x,y,q=0.2){
   return(rbfd)
 }
 
-#calculating the kernel matrix
-kernelmatrix=matrix(0,nrow(data),nrow(data))
 
-for(i in 1:nrow(data)){
-  for(j in 1:nrow(data)){
-    kernelmatrix[i,j]=rbf(data[i,1:(ncol(data)-1)],data[j,1:(ncol(data)-1)],q=0.2)
+###### yet another alternative func rbf & kernel matrix ###### 
+rbfkernelMx <-function(X, sigma = 1, Y = NULL){
+    # test if X is a matrix
+    if(!is.matrix(X))
+    {
+      print("X must be a matrix containing samples in its rows")
+      return()
+    }
+    # test if sigma is a number and > 0
+    if(length(sigma) != 1 || sigma <= 0)
+    {
+      print("sigma must be a number > 0 specifying the rbf-kernel width")
+      return()
+    }
+    if(!is.null(Y))
+    {
+      # test if Y is a matrix
+      if(!is.matrix(Y))
+      {
+        print("Y must be a matrix containing samples in its rows or NULL if it should not be used")
+        return()
+      }
+      # test if vectors in X and Y have same dimension
+      if(ncol(X) != ncol(Y))
+      {
+        print("The samples in the rows of X and Y must be of same dimension")
+        return()
+      }
+    }
+    
+    n <- nrow(X) # number of samples in X
+    
+    if(is.null(Y))
+    {
+      # calculate distance matrix
+      XtX <- tcrossprod(X)
+      XX <- matrix(1, n) %*% diag(XtX)
+      D <- XX - 2*XtX + t(XX) # distance matrix
+    }
+    else
+    {
+      m <- nrow(Y) # number of samples in Y
+      # calculate distance matrix (between vectors of X and Y)
+      XX <- matrix(apply(X ^ 2, 1, sum), n, m)
+      YY <- matrix(apply(Y ^ 2, 1, sum), n, m, byrow = TRUE)
+      XY <- tcrossprod(X, Y)
+      D <- XX - 2*XY + YY
+    }
+    
+    # calculate rbf-kernel matrix
+    K <- exp(-D/(2*sigma))
+    
+    return(K)
   }
-}
 
 
 ######################################################
@@ -129,9 +176,27 @@ kernel.kmeans <- function(
 }
 
 #######  Test functions ###### 
+
+data(spirals)
+data <- spirals
+rm(spirals)
+
+##### Option A 
+#calculating the kernel matrix
+kernelmatrix=matrix(0,nrow(data),nrow(data))
+for(i in 1:nrow(data)){
+  for(j in 1:nrow(data)){
+    kernelmatrix[i,j]=rbf(data[i,1:(ncol(data)-1)],data[j,1:(ncol(data)-1)],q=32)
+  }
+}
 K <- kernelmatrix
+rm(kernelmatrix)
+
+##### Option B
+K <- rbfkernelMx ( X= data, sigma = 0.08)
+
 ###### agains normal kmeans with kernel=Identity
-clusters <- 10
+clusters <- 2
 # Mock assignments
 assignments <- matrix(0, nrow = nrow(K), ncol = clusters)
 part <- nrow(K)/clusters +1
@@ -147,6 +212,9 @@ target <- apply(Z, 1, which.max)
 centers <- (t(assignments) %*% as.matrix(data[,1:2])) / apply(assignments, 2, sum)
 kmns <- kmeans(data[,1:2], iter.max = 10,centers = centers)
 # See cluster distribution on mock data to see their are equal
-plot(data$x1, data$x2, col=target)
-plot(data$x1, data$x2, col=kmns$cluster)
+#plot(data$x1, data$x2, col=target)
+#plot(data$x1, data$x2, col=kmns$cluster)
+# plot spiral data 
+plot(data[,1], data[,2], col=target, main="Custom Kernel K-means sigma 0.08")
+plot(data[,1], data[,2], col=kmns$cluster, main="K-means")
 
