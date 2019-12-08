6696a5e7d6a9726f54c9e92a00f661e18992703c
mariagk <maria.gotsopoulou@hotmail.com>
2019-12-08 18:42:06 +0100

    completed model tuning functions and spectral clustering

--- code/SpectralClustering.R
+++ code/SpectralClustering.R
@@ -1,72 +1,33 @@
 
 spectralClustering <- function(
   S, #similarity matrix, S,
-  n.neighboors=round(log(dim(S)[1])), #choose k as per the assymptotic connectivity results
   clusters, # Number of clusters
   laplacian = "normalizedRandomWalk", # or unnormalized,normalizedSymmetric
-  method = "kmeans" #pam or discretization
+  nrandomSet 
   ){
-  # transform a given set of x_1,...,x_n with pairwise similarities S into a graph
-  # connect vertex v_i with v_j if v_j is among the k-nearest neighboor of v_i
-  # leads to a directed graph since neighboorhood relationship is not symmetric
-  N <- length(S[,1])
-  if (n.neighboors >= N) {  # fully connected
-    A <- S
-  } else {
-    # A must be made of positive values and be symmetric.
-    A <- matrix(rep(0,N^2), ncol=N)
-    for(i in 1:N) { # for each line
-      # only connect to those points with larger similarity
-      best.similarities <- sort(S[i,], decreasing=TRUE)[1:n.neighboors]
-      for (s in best.similarities) {
-        j <- which(S[i,] == s)
-        A[i,j] <- S[i,j]
-        # solution applied to make the graph undirected so A is symmetric 
-        # if A_ij is selected as a nearest neighboor, so will A_ji.
-        A[j,i] <- S[i,j] 
-      }
-    }
-  }
   # degree matrix D where each diagonal value is the degree of the respective vertex 
   # and all other positions are zero
-  D <- diag(apply(A, 1, sum))
-  # Calculate the Laplacian,
-  # matrix power operator: computes M^power (M must be diagonalizable)
-  "%^%" <- function(M, power)
-    with(eigen(M), vectors %*% (values^power * solve(vectors)))
-  # which laplacian ? look at the degree distribution of the similarity graph
+  D <- diag(apply(S, 1, sum))
+  # Calculate the Laplacian
+  # which laplacian should we pick? look at the degree distribution of the similarity graph
   if(laplacian=="unnormalized"){ # Shi and Malik (2000)
-    L <- D - A
+    L <- D - S
   } else if(laplacian == "normalizedSymmetric"){ #Ng, Jordan, and Weiss (2002)
-    L <- (D %^% (-1/2)) %*% A %*% (D %^% (-1/2))
-    # additional multiplication D might lead to undesired
+    L <- .sqrtm(D) %*% S %*% .sqrtm(D)
+    # additional multiplication D might lead to undesired artifacts
   } else if(laplacian=="normalizedRandomWalk"){ # preferred by Von Luxburg, U. (2007). 
     # cluster indicator vectors 1_{A_i}
-    L <- (D %^% (-1)) %*% A
+    L <- .ginv(D) %*% S
   }
   # Compute eigenvectors and eigenvalues of L
-  # since we use the k nearest neighbor graph obtained Laplacian will be sparse
   evL <- eigen(L, symmetric=TRUE)
   # choose eigenvectors
   n <- ncol(S)
   Z <- evL$vectors[,((n-clusters+1):n)]
-  if(method=="kmeans"){
-    C<- kmeans(Z, centers=clusters, nstart=5)
-    classes <- C$cluster
-  }else if(method=="pam"){
-    classes <- cluster::pam(x = Z, k = clusters,cluster.only=TRUE,diss = FALSE)
-  }else if(method=="discretization"){
-    res <- sort(abs(evL$values),index.return = TRUE)
-    U <- evL$vectors[,res$ix[1:clusters]]
-    normalize <- function(x) x / sqrt(sum(x^2))
-    if(laplacian == "normalizedSymmetric"){
-      U = t(apply(U,1,normalize))
-    }
-    eigDiscrete = .discretisation(U)
-    eigDiscrete = eigDiscrete$discrete
-    classes = apply(eigDiscrete,1,which.max)
-  }
-  assignments <- matrix(0, nrow = length(classes), ncol = length(unique(classes)))
+  # run K-means algorithm on that space to get k clusters 
+  C <- kmeans(Z, centers=clusters,nstart = nrandomSet, iter.max =100) 
+  classes <- C$cluster
+  assignments <- matrix(0, nrow = length(classes), ncol = clusters)
   for(i in 1:length(classes)) {
     assignments[i,classes[i]] <- 1
   }
@@ -74,63 +35,30 @@ spectralClustering <- function(
 }
 
 
-######## Internal Functions ##############
-
-.discretisation <- function(eigenVectors) {
-  
-  normalize <- function(x) x / sqrt(sum(x^2))
-  eigenVectors = t(apply(eigenVectors,1,normalize))
-  
-  n = nrow(eigenVectors)
-  k = ncol(eigenVectors)
-  
-  R = matrix(0,k,k)
-  R[,1] = t(eigenVectors[round(n/2),])
-  
-  mini <- function(x) {
-    i = which(x == min(x))
-    return(i[1])
-  }
-  
-  c = matrix(0,n,1)
-  for (j in 2:k) {
-    c = c + abs(eigenVectors %*% matrix(R[,j-1],k,1))
-    i = mini(c)
-    R[,j] = t(eigenVectors[i,])
-  }
-  
-  lastObjectiveValue = 0
-  for (i in 1:20) {
-    eigenDiscrete = .discretisationEigenVectorData(eigenVectors %*% R)
-    
-    svde = svd(t(eigenDiscrete) %*% eigenVectors)
-    U = svde[['u']]
-    V = svde[['v']]
-    S = svde[['d']]
-    
-    NcutValue = 2 * (n-sum(S))
-    if(abs(NcutValue - lastObjectiveValue) < .Machine$double.eps) 
-      break
-    
-    lastObjectiveValue = NcutValue
-    R = V %*% t(U)
-    
-  }
-  
-  return(list(discrete=eigenDiscrete,continuous =eigenVectors))
+.ginv <- function (X, tol = sqrt(.Machine$double.eps))
+{
+  if (length(dim(X)) > 2 || !(is.numeric(X) || is.complex(X)))
+    stop("'X' must be a numeric or complex matrix")
+  if (!is.matrix(X))
+    X <- as.matrix(X)
+  Xsvd <- svd(X)
+  if (is.complex(X))
+    Xsvd$u <- Conj(Xsvd$u)
+  Positive <- Xsvd$d > max(tol * Xsvd$d[1], 0)
+  if (all(Positive))
+    Xsvd$v %*% (1/Xsvd$d * t(Xsvd$u))
+  else if (!any(Positive))
+    array(0, dim(X)[2:1])
+  else Xsvd$v[, Positive, drop = FALSE] %*% ((1/Xsvd$d[Positive]) * t(Xsvd$u[, Positive, drop = FALSE]))
 }
 
-.discretisationEigenVectorData <- function(eigenVector) {
-  
-  Y = matrix(0,nrow(eigenVector),ncol(eigenVector))
-  maxi <- function(x) {
-    i = which(x == max(x))
-    return(i[1])
-  }
-  j = apply(eigenVector,1,maxi)
-  Y[cbind(1:nrow(eigenVector),j)] = 1
-  
-  return(Y)
-  
+.sqrtm <- function(x)
+{
+  tmpres <- eigen(x)
+  V <- t(tmpres$vectors)
+  D <- tmpres$values
+  if(is.complex(D))
+    D <- Re(D)
+  D <- pmax(D,0)
+  return(crossprod(V*sqrt(D),V))
 }
-
--- code/kernelProject.R
+++ code/kernelProject.R
@@ -2,7 +2,7 @@
 #PROCEDURE....= 1. Tune Kernel for Kernel-Kmeans and Spectral Clustering    #
 #               2. Test performance comparing Variation of Inforation with  #
 #                    the true partition of the data                         #
-#INPUT DATA...= Pre-processed Reutres data                                  #
+#INPUT DATA...= Pre-processed Reuters data                                  #
 #R VERSION....= R version 3.5.1 (2018-07-02) Feather Spray                  # 
 #AUTHOR.......= Maria Gkotsopoulou & Ricard Monge Calvo                     #
 #CREATED......= December 2019                                               #
@@ -14,6 +14,7 @@
 
 library(dplyr)
 library(kernlab)
+library(tm)
 set.seed(42)
 
 ######################################################
@@ -38,28 +39,24 @@ reutrain <- read.delim("data/r52-train-no-stop.txt", header = FALSE,
                        stringsAsFactors = FALSE)
 reutest <- read.delim("data/r52-test-no-stop.txt", header = FALSE,
                       stringsAsFactors = FALSE)
-
-# We keep the topics acq, trade and ship 
-topics <- c("acq", "trade", "ship")
+reuters <- rbind(reutrain ,reutest) 
+rm(reutrain, reutest)
+# Get topic counts to select topic
+topic.counts <- reutrain %>% group_by(Topic) %>% summarise(Count=n()) %>% arrange(desc(Count))
+# We keep the topics crude, trade and ship 
+topics <- c("crude", "trade", "ship")
 # Own colnames
 cols <- c("Topic", "Document")
 # Change cols and filter topics
-colnames(reutrain) <- cols
-colnames(reutest) <- cols
-reutrain <- reutrain %>% filter(Topic %in% topics)
-reutest <- reutest %>% filter(Topic %in% topics)
-
-# Get topic counts to select topic
-topic.counts <- reutrain %>% group_by(Topic) %>% summarise(Count=n()) %>% arrange(desc(Count))
+colnames(reuters) <- cols
+reuters <- reuters %>% filter(Topic %in% topics) %>% 
+              mutate(Topic=factor(Topic))
 # Get number of instances and topics
-N <- dim(reutrain)[1]
-k <- length(unique(reutrain$Topic))
-# Shuffle data
-set.seed(42)
-reutrain <- reutrain[sample(1:N, N),]
+N <- dim(reuters)[1]
+k <- length(unique(reuters$Topic))
 
 ######################################################
-#################   MODEL TUNING    ##################
+#########   MODEL TUNING Functions   #################
 ######################################################
 # As string kernel we use Kernlab's version of Normalized Boudrange kernel
 # which computes the appearances of subsequences of length less or
@@ -67,55 +64,108 @@ reutrain <- reutrain[sample(1:N, N),]
 # product between those appearances vectors.
 # To tune the hyperparameter *length* we use 10-fold CV with a range spaces
 # going from 2-21 length.
-cv <- 10
-# Get gridspace to use in parameter tuning
-grid.space <- data.frame(length=2:10)
+
 # Get control function to use in parameter tuning by choosing measure in 
 # assigment.perfomance function
 metric <- "vi"
+# Control function to compute performance, which recieves the.
+# output of method and the reference object and returns a performance
+# measure that is a number.
 control <- function(Z, R) {
-   perf <- assignment.performance(Z, R)
-   mes <- perf[metric]
-   return(mes)
+  perf <- assignment.performance(Z, R)
+  mes <- unlist(perf[metric])
+  return(mes)
 }
+
+clusteringMethod <- function(
+  method,
+  K, # kernelMatrix
+  nclust, # Number of cluster for method
+  nCV){
+  if(method=="kernel.kmeans"){
+    Z <- kernel.kmeans(K, clusters=nclust,nrandomSet = nCV)
+  }
+  if(method=="spectralClustering"){
+    Z <- spectralClustering(S=K, clusters=nclust,nrandomSet= nCV )
+  }
+  return(Z)
+}
+
 kernel.tune <- function(
   data, # Dataset to use in the tuning procedure
-  cv=10, # Folds to use in K-fold CV
+  clusters ,
+  cv, # Folds to use in K-fold CV
   grid.space, # Dataframe with parameters, where each row is turned into a list
               # of parameters for method function.
-  method, # Function to compute Assignment object to compare to Reference.
-  reference, # Reference object to use with control function to compare to model
-  control, # Control function to compute performance, which recieves the.
-          # output of method and the reference object and returns a performance
-          # measure that is a number.
-  clusters=k # Number of cluster for method
+  # Function to compute Assignment object to compare to Reference.
+  clMethod= c("kernel.kmeans", "spectralClustering"), 
+  reference # Reference object to use with control function to compare to model
 ) {
-  out.cv <- grid.space %>% mutate(CV=0)
+  out.cv <- grid.space %>% 
+              mutate(type="spectrum",
+                     normalized = TRUE,
+                     CVkk = 0, 
+                     CVspecc = 0)
   # List parameters from grid.space
   parameters <- split(grid.space, seq(nrow(grid.space)))
   for(i in 1:nrow(grid.space)) {
     # Get parameters grom grid
     param <- parameters[[i]]
-    # Additional parameters for kernel definition
-    param$type <- "spectrum"
-    param$normalized <- TRUE
     # Get kernel function with parameters
     skernel <- do.call(stringdot, param)
     # Init df to keep cv performances
     aux.cv <- rep(0,cv)
-    # Split data into folds
-    folds <- createFolds(data,cv)
-    for(j in 1:cv) {
-      # For each fold create KernelMatrix
-      fold.ind <- folds[[j]]
-      train <- data[-fold.ind]
-      K <- kernelMatrix(skernel, train)
-      Z <- method(K, clusters=clusters)
+    K <- kernelMatrix(skernel, data)
+    for(x in 1:length(clMethod)){
+      Z <- clusteringMethod(method=clMethod[x], K , nclust =clusters,nCV=cv )
       perf <- control(Z, reference)
-      aux.cv[j] <- perf
+      if(clMethod[x]=="spectralClustering"){
+        out.cv$CVspecc[i] <- perf
+      }
+      if(clMethod[x]=="kernel.kmeans"){
+        out.cv$CVkk[i] <- perf 
+      }
     }
-    out.cv$CV[i] <- mean(aux.cv)
   }
   return(out.cv)
 }
+
+
+######################################################
+##################   MODEL TUNING    #################
+######################################################
+tuneData <- reuters$Document
+clustMethod <- c("spectralClustering")
+gridSpace <- data.frame(length=c(2,5,10,15,20))
+ref <- assignment.matrix(reuters$Topic)
+
+kernelTuneCV <- kernel.tune(data = tuneData ,
+                            clusters = k,
+                            cv=10,
+                            grid.space =  gridSpace,
+                            clMethod= clustMethod,
+                            reference =ref )
+
+######################################################
+##################   K Means    #################
+######################################################
+
+# Transform data to DocumentTermMatrix for regular k-means
+# with weighting given by the term frequency–inverse document frequency
+# and reduce dimensions allowing only up to 0.95 matrix sparsity
+corpus <- VCorpus(VectorSource(reuters$Document))
+dt.mat <- DocumentTermMatrix(corpus,
+                             control = list(weighting = weightTfIdf)) %>% 
+  removeSparseTerms(sparse = 0.95)
+
+
+## plot 
+
+## best lambdas
+
+## do clustering method using customs functions
+
+## with Z do assignment.performance
+                            
+  
 
